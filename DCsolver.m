bc2c5b6758f0754539355857d3553136a88f8b7b
Crrrrrayon <53397399+Crrrrrayon@users.noreply.github.com>
2025-07-17 20:30:49 +0800

    Update DCsolver.m

--- DCsolver.m
+++ DCsolver.m
@@ -1,11 +1,18 @@
-function [ X,T ] = DCsolver(D,rho,T0,a,r)
+function [X, T] = DCsolver(D, rho, T0, a, r)
+[U, S, V] = svd(D, 'econ');
+S_vec = diag(S);  
+k = length(S_vec);
 
-[U,S,V] = svd(D,'econ');
+if length(T0) < k
+    T0 = [T0; zeros(k - length(T0), 1)];
+elseif length(T0) > k
+    T0 = T0(1:k);
+end
 
+% DCInner
 for t = 1:100
-    
-    [ X,T1 ] = DCInner(S,rho,T0,a,U,V,r);
-    err = sum((T1-T0).^2);
+    [X, T1] = DCInner(S_vec, rho, T0, a, U, V, r);
+    err = sum((T1 - T0).^2);
     if err < 1e-6
         break
     end
@@ -15,16 +22,26 @@ T = T1;
 end
 
 
-function [ X,t ] = DCInner(S,rho,J,epislon,U,V,r)
-lambda=1/2/rho;
-S0 = diag(S);
-grad=(exp(epislon).*epislon)./(epislon+J).^2;
-for i=1:length(S0)
-    if i<=r
-        t(i)=max(S0(i)-lambda*grad(i),0);
+function [X, t] = DCInner(S_vec, rho, J, epsilon, U, V, r)
+lambda = 1/(2*rho);
+k = length(S_vec);  
+
+if length(J) < k
+    J = [J; zeros(k - length(J), 1)];
+elseif length(J) > k
+    J = J(1:k);
+end
+
+grad = (exp(epsilon).*epsilon)./(epsilon + J).^2;
+
+t = zeros(k, 1);
+for i = 1:k
+    if i <= min(r, k)  
+        t(i) = max(S_vec(i) - lambda*grad(i), 0);
     else
-        t(i)=0;
+        t(i) = 0;
     end
 end
-X=U*diag(t)*V';
+
+X = U * diag(t) * V';
 end
